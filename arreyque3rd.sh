1bf6ab3abe75d8d8cdb370cba664a2a1b270af7f
poojaamar <pamar6401@gmail.com>
2022-07-04 17:48:04 +0100

    day7 files added

---
+++ arreyque3rd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+read -p "Enter Number : " num
+count=0
+for ((i=2;$num>1;i++))
+do
+   while [ $(($num%$i)) -eq 0 ]
+   do
+	primefactor[((count++))]=$i
+        num=$(($num/$i))
+    done
+
+done
+
+echo ${primefactor[@]}
---
+++ arreyque4th.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+
+Number=(-10 -20 30)
+
+total=$(( Number[0] + Number[1] + Number[2] ))
+echo $Total
